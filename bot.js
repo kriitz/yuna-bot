0229513456930bc0ae1b278e759c37798a3a0f73
ImKritz <philip.thinh.nguyen@gmail.com>
2018-10-25 05:49:31 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+//
+const Yuna = require("./src/client.js");
+
+// [[ Imports ]] const  = require('./src/commands/.js');
+const WarnCommand = require('./src/commands/warn.js');
+
+bot.once("ready", function () {
+	bot.user.setGame("Witchcraft | //help");
+
+	const rawCommands = [
+		WarnCommand,
+  }
+  
+	bot.initializeCommands(rawCommands);
+	console.log("Ready to begin!");
+});
+
+bot.login(process.env.BOT_TOKEN);
+    
