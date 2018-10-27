9e1e979295cf384bcc48eca3cbad9c4ce670a36a
ImKritz <philip.thinh.nguyen@gmail.com>
2018-10-26 21:04:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,5 +19,40 @@ bot.once("ready", function () {
 	console.log("Ready to begin!");
 });
 
+bot.on("message", function (msg) {
+	if (msg.author == bot.user)
+		return
+
+	if(msg.author.id != bot.user.id && msg.content.startsWith("/")){
+		var cmdTxt = msg.content.split(" ")[0].substring(2);
+        var suffix = msg.content.substring(cmdTxt.length + 3);
+		var cmd = bot.commands()[cmdTxt];
+
+		if(cmd){
+			if (cmd.hasPermission(msg)){
+				try{
+					const replyContent = cmd.process(msg, suffix);
+					if (replyContent) msg.reply(replyContent);
+				} catch(error){
+					console.log("Command " + cmdTxt + " failed. \n" + error.stack);
+				}
+				console.log(cmdTxt + " command excuted.");						
+			}else{
+				msg.reply("you do not have permission to use that command.");
+			}
+		}
+	}else{
+		if (msg.mentions.users.first() !== bot.user){
+			for (var index in blockedWords){
+				if(msg.content.toLowerCase().match(blockedWords[index])){
+					msg.reply("Your message was deleted, we do not condone dark humor or loliconic pictures on our server. \n\t" + msg.author.username + " you have been warned.")
+					msg.delete();
+				}	
+			}
+		}else{
+			msg.reply("You mentioned me!");
+		}
+    }
+});
+
 bot.login(process.env.BOT_TOKEN);
-    
