7262fd34ce97cbbd6375a0c5a1a6714db44943d0
ImKritz <philip.thinh.nguyen@gmail.com>
2018-10-30 13:18:47 -0700

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -21,7 +21,7 @@ module.exports = class HelpCommand extends Command{
 	}
 
 	process(msg, suffix){
-		var replyContent = "Hello, I'm a Yuna bot created by Philip 'Kritz' Nguyen in the home of Witchcraft!\nRole has specific commmands you can use. \n\t Use the command '//commands' to see all your available commands!";
+		var replyContent = "Hello, I'm a Yuna bot created by Philip 'Kritz' Nguyen in the home of Witchcraft!\nRole has specific commands you can use. \n\t Use the command '/commands' to see all your available commands!";
 		if (suffix.startsWith('-c'))
 			replyContent = "The prefix for all commands is `//`.\nThe command name comes after the prefix i.e: \n`//command`.\nMost commands require a suffix to run properly and that comes after the command name with a space i.e: \n`//command help`.\nIf there are options on the command you want to use, you add it to the command call with a `-` after the command name but before the suffix. I.e: \n`//command -r help`";
 		
