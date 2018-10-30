8bd20f7503899a2b853ff1ffeea1263251f5a3c5
ImKritz <philip.thinh.nguyen@gmail.com>
2018-10-30 16:49:16 -0700

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -23,9 +23,11 @@ module.exports = class CommandsCommand extends Command{
     	const commands = this.bot.commands();
     	var info = "More infomation about a specific command use `/how <Command Name>`\n"
     		+ `__**Available Yuna Commands:**__ \n\n`
-
+	var printedCmds = [];
+	
     	for(var cmd in commands){
-    		if (commands[cmd].hasPermission(msg) && commands[cmd].isAlias == false){
+    		if (commands[cmd].hasPermission(msg) && printedCmds.includes(commands[cmd]) == false){
+			printedCmds.push(commands[cmd]);
 	    		info += "**" + cmd + ":**";
 	    		
 	    		var usage = commands[cmd].get('usage');
