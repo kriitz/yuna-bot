fe48e905c0256fdb3c0e82ebd027c13ee62b59b8
ImKritz <philip.thinh.nguyen@gmail.com>
2019-08-29 15:09:04 -0700

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const firebase = require("firebase");
-const twilio = require('twilio')(accountSid, authToken);
+const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 twilio.messages.create({
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+19712323401',
+    from: TWILIO_PHONE_NUMBER,
     to: '+15037299487'
    }).then(message => console.log(message.sid));
 
