ccc02bd8c15bd02e4d46a34630471458b842799b
ImKritz <philip.thinh.nguyen@gmail.com>
2019-08-30 20:39:32 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,6 +13,6 @@ app.post('/sms', (req, res) => {
 	res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
+http.createServer(app).listen(process.env.PORT || 1337, () => {
 	console.log('Express server listening on port 1337');
 });
